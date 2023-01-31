49b624bed0acc0cf0916dea7e90d2ece912411a4
donovan-cartier <donovan.cartier0@gmail.com>
2023-01-31 23:07:58 +0100

    début missile + boost

--- scenes/ItemBox.gd
+++ scenes/ItemBox.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-enum ITEMS {MISSILE}
-var item
+var ITEMS = ["MISSILE", "BOOST"]
+var item_in_box
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	item = ITEMS.MISSILE
-
+	item_in_box = ITEMS[1]
+	print(item_in_box)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -15,4 +15,5 @@ func _ready():
 
 func _on_ItemBox_area_entered(area):
 	if area.is_in_group("player"):
+		area.get_item(item_in_box)
 		queue_free()
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -15,12 +15,18 @@ var current_move_mode = move_mode.NORMAL
 var color
 var id
 var player_name
+var items = []
+var is_aiming_missile = false
+
+var missile = load("res://scenes/items/Missile.tscn")
 
 var progress_hud
 
 onready var gamerules = get_tree().get_root().get_node("Track/Gamerules")
 
 onready var course_path = get_parent()
+
+var cursor_aim = load("res://ressources/cursor_aim_32.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,6 +36,7 @@ func _ready():
 	$PlayerInfo/PlayerName.text = player_name
 	id = get_tree().get_nodes_in_group("player").size()
 	print("player " + str(color) + " => ID: " + str(id))
+	$AnimationPlayer.play("Move")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -42,6 +49,17 @@ func _process(delta):
 				course_path.offset += speed * delta
 		move_mode.ALTERNATE:
 			pass
+	
+	if Input.is_action_just_pressed("use_item"):
+		if is_aiming_missile:
+			fire_missile()
+		elif !items.empty():
+			var item_to_use = items[0]
+			match item_to_use:
+				"MISSILE":
+					use_missile()
+				"BOOST":
+					use_boost()
 				
 func pass_lap():
 	current_lap += 1
@@ -59,3 +77,45 @@ func set_movement(mode):
 			$PlayerInfo/Spacebar.visible = true
 		"ALTERNATE":
 			current_move_mode = move_mode.ALTERNATE
+
+
+
+
+################ITEMS################
+func get_item(item):
+	print(item)
+	items.append(item)
+	
+func remove_item(id = 0):
+	items.remove(id)
+	
+#MISSILE
+func use_missile():
+	Input.set_custom_mouse_cursor(cursor_aim)
+	is_aiming_missile = true
+
+func fire_missile():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var missile_instance = missile.instance()
+	missile_instance.global_position = mouse_pos
+	get_tree().get_root().get_node("Track").add_child(missile_instance)
+	print("missile shot")
+	is_aiming_missile = false
+	Input.set_custom_mouse_cursor(null)
+	remove_item()
+
+#BOOST
+func use_boost():
+	speed = 300
+	var boost_timer = Timer.new()
+	boost_timer.wait_time = 1
+	boost_timer.connect("timeout", self, "_boost_timeout", [boost_timer])
+	add_child(boost_timer)
+	boost_timer.start()
+	remove_item()
+
+func _boost_timeout(timer):
+	print("timeout")
+	timer.queue_free()
+	speed = default_speed
+
